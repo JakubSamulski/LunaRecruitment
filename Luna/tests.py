c66f11bb22ff983a9578c068582a9e78ca164a16
Kuba <kubasamulski1@gmail.com>
2025-02-20 21:47:41 +0100

    basic test

--- Luna/tests.py
+++ Luna/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.urls.base import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+
+class HydroponicSystemTests(APITestCase):
+    fixtures = ["Luna/fixtures/test.json"]
+
+    # Authentication Tests
+    def test_list_systems_unauthenticated(self):
+        self.client.logout()
+        response = self.client.get(reverse("hydroponic system-list"))
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
