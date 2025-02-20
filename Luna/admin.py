d2cf6f09ef721717abe603c21771d2a69644bbb0
Kuba <kubasamulski1@gmail.com>
2025-02-20 19:41:32 +0100

    added app to settings and generated admin pages

--- Luna/admin.py
+++ Luna/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from Luna.models import HydroponicSystem, Reading
+
+
 # Register your models here.
+@admin.register(HydroponicSystem)
+class HydroponicSystemAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Reading)
+class ReadingAdmin(admin.ModelAdmin):
+    pass
