10668946350d2b6cbbf00d71a02ec2e6fe98d2be
Kuba <kubasamulski1@gmail.com>
2025-02-20 19:36:46 +0100

    added models.py

--- Luna/models.py
+++ Luna/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+class HydroponicSystem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Reading(models.Model):
+    hydroponic_system = models.ForeignKey(HydroponicSystem, on_delete=models.CASCADE)
+    temperature = models.FloatField()
+    ph = models.FloatField()
+    tds = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.hydroponic_system.name} - {self.timestamp}"
