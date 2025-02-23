795125e02e2f9abe36e32a793e94ff9cead132d1
JakubSamulski <kubasamulski@gmail.com>
2025-02-23 01:15:32 +0100

    added api docs

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -19,8 +19,6 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 
-from rest_framework import permissions
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
